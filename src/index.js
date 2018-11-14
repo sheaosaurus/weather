17357492e25437d94dea3c392964e1d479ac26cc
Shea Long <sheaglong@gmail.com>
2018-11-14 15:22:47 -0500

    First commit - adding project files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.css';
+import App from './components/App.jsx';
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
